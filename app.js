271e82bae85d90a34fa0f04f3698e4be45e6f5ac
tuğrul kaan kılıç <tkaankilic@gmail.com>
2022-08-17 21:06:40 +0300

    döngüler

--- app.js
+++ app.js
@@ -296,3 +296,41 @@ const gider = Number(prompt("Giderlerinizi giriniz"));
 const gelir = Number(prompt("Gelirleriniz giriniz"));
 const kredi = gelir - gider >= 5000 ? "KREDI ALABILIR 🤑" : " KREDI ALAMAZ 🥺";
 console.log(kredi);
+
+
+
+// * ============================================
+// *            DONGULER(LOOPS)- FOR
+// * ============================================
+
+console.log("****** FOR *******");
+
+//? Ornek: 1 den n kadar sayiları toplayan kodu yaziniz.
+
+const n = Number(prompt("Bir sayi girniz:"));
+
+let toplam = 0;
+for (let i = 1; i <= n; i++) {
+  // toplam = toplam + i;
+  toplam += i;
+  console.log(toplam, i);
+}
+
+console.log("SONUC:", toplam);
+
+//? Ornek: 0-100 Arasinda 10 adet rasgele tamsayi sayi ureten kodu
+//? for dongusu ile yaziniz..
+
+for (let i = 1; i <= 10; i++) {
+  const rasgele = Math.round(Math.random() * 100);
+  console.log(rasgele);
+}
+
+//* NOT:
+//* Math.floor() =>en yakin alt tamsayiya yuvarlar
+//* Math.ceil() =>en yaki ust tamsayiya yuvarlar
+//* Math.round() => degerine gore yuvarlar.
+//* Math.trunc() => kesirli sayinin tam kismini alir.
+
+//? ÖRNEK: Girilen bir sayinin Asal olup olmadigini yazdiran kodu
+//? for dongulerini kullanarak yaziniz.
