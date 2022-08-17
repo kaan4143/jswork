1f5b7a715a9b89fbabf4f1df7e8e2eb108c18ee1
tuğrul kaan kılıç <tkaankilic@gmail.com>
2022-08-17 14:26:56 +0300

    js devam

--- app.js
+++ app.js
@@ -150,3 +150,149 @@
 
 // let sonuç = a%10 === b%10 || a%10 === c%10 || b%10 === c%10;
 // console.log(`girilen üç sayıdan en az iki tanesinin  1. basamklarının eşit olma durumu ${sonuç}`)
+
+// // * ===============================================
+// // *                  KARAR YAPILARI
+// // * ===============================================
+
+// //**************** IF-ELSE *******************
+
+// console.log("****** CONDITIONS *******");
+
+// //? ORNEK1: Console'dan girilen bir sayisinin pozitif,negatif
+// //? yada 0 oldugunu tespit ederek yazdiriniz.
+
+// const sayi = Number(prompt("Bir sayi giriniz:"));
+// if (sayi > 0) {
+//   console.log(`${sayi} pozitiftir.`);
+// } else if (sayi == 0) {
+//   console.log(`${sayi}'a esittir.`);
+// } else {
+//   console.log(`${sayi} negatiftir.`);
+// }
+// console.log(sayi, typeof sayi);
+
+
+// //? ORNEK2: console'dan 3 tamsayi alarak bunlarin en buyugunu
+// //? yazdiriniz.
+
+// const n1 = +prompt("sayi1")
+// const n2 = +prompt("sayi2")
+// const n3 = +prompt("sayi3")
+
+// 1.yöntem
+
+// if( n1>= n2 && n1 >= n3){
+//     console.log(`${n1} en büyük sayıdır`);
+// }
+// else if( n2>= n1 && n2 >= n3){
+//     console.log(`${n2} en büyük sayıdır`);
+// }
+// else if( n3 >= n2 && n3 >= n1){
+//     console.log(`${n3} en büyük sayıdır`);
+// }
+
+// 2.yöntem
+
+// let enbuyuk = n1;
+// if (n2 >= enbuyuk) {
+//     enbuyuk = n2;
+// }
+
+// if (n3 >= enbuyuk) {
+//     enbuyuk = n3
+// }
+
+// console.log(`${enbuyuk} en büyüktür`);
+
+// 2 sayı için 4 işlem yapabilen hesap makinesi yazınız ödev
+
+// örnek kaldı geçti ternary kullanarak
+
+// const not = +prompt("notunuzu giriniz");
+// let sonuc = not >= 50 ? "gecti" : "kaldı"
+// console.log(sonuc)
+
+// 2.yöntem
+
+// const not = +prompt("notunuzu giriniz");
+// console.log(not >= 50 ? "gecti" : "kaldı");
+
+const yas = 17;
+const cinsiyet = "erkek";
+const saglikli = true;
+
+yas >= 18 && cinsiyet === "erkek" && saglikli
+
+//! ODEV1: Dort Islem Hesap Makinasi (2 Sayı bir operator)
+
+console.log("----------------------------------");
+console.log(" ***** Basit Hesap Makinasi ****** ");
+console.log("----------------------------------");
+const s1 = Number(prompt("1.Sayiyi Giriniz:"));
+const islem = prompt("Islemi giriniz: +,-,*, /");
+const s2 = +prompt("2.Sayiyi Giriniz:");
+let netice = 0;
+switch (islem) {
+  case "+":
+    netice = s1 + s2;
+    break;
+  case "-":
+    netice = s1 - s2;
+    break;
+  case "*":
+    netice = s1 * s2;
+    break;
+  case "/":
+    netice = s1 / s2;
+    break;
+  default:
+    alert("Yanlis islem girisi");
+    break;
+}
+console.log(`${s1} ${islem} ${s2} = ${netice}`);
+
+//! ODEV2 :Clarusway’deki haftalık ders ve etkinlik programınızı, console’dan girilen gün değerine göre çıktı veren kodu switch-case yapısı ile yazınız.
+//? Pazartesi, Salı ,Çarşamba, Perşembe -> InClass
+//? Cuma -> Teamwork
+//? Cumartesi ->  InClass + Workshop
+//? Pazar -> Self-Study
+//? Aksi takdirde -> Yanlis gun girildi.
+
+const gun = prompt("Lutfen bir gun giriniz: ").toLowerCase();
+switch (gun) {
+  case "pazartesi":
+  case "sali":
+  case "carsamba":
+  case "persembe":
+    console.log("Inclass");
+    break;
+  case "cuma":
+    console.log("Teamwork");
+    break;
+  case "cumartesi":
+    console.log("Inclass ve workshop");
+    break;
+  case "pazar":
+    console.log("Self study.");
+    break;
+  default:
+    console.log("Yanlis gun girildi");
+}
+
+//! ODEV3:Maasi asgari ucretten az olanlara %50 zam,fazla olanlara ise %10 zam yapmak istiyoruz.
+const maas = Number(prompt("Maasinizi giriniz"));
+const asgariUcret = 5500;
+const zamliMaas = maas >= asgariUcret ? maas * 1.1 : maas * 1.5;
+console.log("NORMAL MAAS:", maas, "ZAMLI MAAS:", Math.trunc(zamliMaas));
+
+//!ODEV4: Kredi Risk Programı
+//? Console’dan kişinin gelir ve gider miktarını alan
+//? eğer kişinin geliri giderinden en az asgari ücret kadar fazla ise Kredi Verilebilir 🤑
+//? değilse Kredi Verilemez 🥺
+//? şeklinde çıktı veren kodu Ternary deyimi kullanarak yazınız.
+
+const gider = Number(prompt("Giderlerinizi giriniz"));
+const gelir = Number(prompt("Gelirleriniz giriniz"));
+const kredi = gelir - gider >= 5000 ? "KREDI ALABILIR 🤑" : " KREDI ALAMAZ 🥺";
+console.log(kredi);
