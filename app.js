5cb54f6c05f0f8cb83e3ca88779dd0a3cda9454e
tuğrul kaan kılıç <tkaankilic@gmail.com>
2022-08-21 14:23:59 +0300

    fonksyonlar

--- app.js
+++ app.js
@@ -402,3 +402,163 @@ Footer
 Footer navigation
 Terms
 Privacy
+
+// ? ===============================================
+// ?               FONKSİYONLAR
+// ? ===============================================
+
+// !------------------------------------------------
+// ! 1.YONTEM  : FUNCTION DECLARATION
+// !------------------------------------------------
+
+console.log("**** FUNC DECLARATION ******");
+
+//* ORNEK:
+//************************************************/
+yazdir();
+
+//! Fonksiyonun tanimlanmasi (declaration)
+function yazdir() {
+  console.log("merhaba");
+}
+
+yazdir(); //! invoke, call, cagirma
+yazdir();
+
+//* ORNEK2:
+//*************************************************/
+
+function selamla(ad, soyAd = "") {
+  console.log(`Merhaba ${ad} ${soyAd}`);
+}
+
+selamla("Can", "Yilmaz");
+selamla("Canan", "Ozturk");
+selamla("Ayse");
+selamla("John");
+
+//! Bir parametreyi cagirma sirasinda kullanmaz isek onun
+//! yerine default parametre atayabiliriz. Ornekteki lastName
+//! parametresi icin default değer olarak '' atanmistir.
+
+//* ORNEK3:
+//***************************************************/
+
+function yasHesapla(isim, dogumTarihi) {
+  // const sonuc = `${isim} in yasi ${2022 - dogumTarihi} dir.`;
+  // const sonuc = `${isim} in yasi ${new Date().getFullYear() - dogumTarihi} dir.`;
+  // return sonuc;
+  return `${isim} in yasi ${new Date().getFullYear() - dogumTarihi} dir.`;
+}
+
+const mesaj1 = yasHesapla("Elif Can", 1990);
+console.log(mesaj1);
+console.log(yasHesapla("Veli Canan", 1980));
+
+//* ORNEK4:
+//*****************************************************/
+
+function tekCift(sayi) {
+  return sayi % 2 ? `${sayi} TEKTIR` : `${sayi} CIFTTIR`;
+}
+
+console.log(tekCift(5));
+console.log(tekCift(2));
+
+//* ORNEK4:
+//*****************************************************/
+const n = +prompt("Bir sayi giriniz:");
+console.log(tekCift(n));
+
+// ? ================================================
+// ?                FONKSİYONLAR
+// ?=================================================
+
+//!--------------------------------------------------
+//! 2.YONTEM  : FUNCTION EXPRESSION
+//!--------------------------------------------------
+
+console.log("******** 2- EXPRESSION*******");
+
+//* ORNEK:
+//***************************************************/
+// function tekCift(sayi) {
+//   return sayi % 2 ? `${sayi} TEKTIR` : `${sayi} CIFTTIR`;
+// }
+
+//! Cannot access 'tekCift1' before initialization  at
+// console.log(tekCift1(9));
+
+//! Funct Expression yontemi ile tanimlama
+const tekCift1 = function (sayi) {
+  return sayi % 2 ? `${sayi} TEKTIR` : `${sayi} CIFTTIR`;
+};
+
+console.log(tekCift1(5));
+
+//* ORNEK: 3 sayinin en büyüğünü bulan fonks.
+//******************************************************/
+
+const buyukBul = function (n1, n2, n3 = -Number.MAX_VALUE) {
+  let enBuyuk;
+  if (n1 >= n2 && n1 >= n3) {
+    enBuyuk = n1;
+  } else if (n2 >= n1 && n2 >= n3) {
+    enBuyuk = n2;
+  } else if (n3 >= n1 && n3 >= n2) {
+    enBuyuk = n3;
+  }
+  return enBuyuk;
+};
+
+// const n1 = +prompt("Sayi1:");
+// const n2 = +prompt("Sayi2:");
+// const n3 = +prompt("Sayi3:");
+
+// console.log(Number.MIN_VALUE);
+// console.log("Girilen sayilarin en buyugu:", buyukBul(n1, n2, n3));
+console.log("Girilen sayilarin en buyugu:", buyukBul(-8, -9, -1));
+console.log("Girilen sayilarin en buyugu:", buyukBul(-8, -9));
+
+console.log("Girilen sayilarin en buyugu:", buyukBul(9, 10));
+
+//* ORNEK: Bir fonksiyon içerisinde başka fonksiyonlar çağrılabilir
+//**************************************************/
+const topla = function (s1, s2) {
+  return s1 + s2;
+};
+
+const cikar = function (s1, s2) {
+  return s1 - s2;
+};
+const bol = function (s1, s2) {
+  return s1 / s2;
+};
+const carp = function (s1, s2) {
+  return s1 * s2;
+};
+
+const hesapla = function (s1, s2, islem) {
+  let sonuc = 0;
+  switch (islem) {
+    case "+":
+      sonuc = topla(s1, s2);
+      break;
+    case "-":
+      sonuc = cikar(s1, s2);
+      break;
+    case "/":
+      sonuc = bol(s1, s2);
+      break;
+    case "*":
+      sonuc = carp(s1, s2);
+      break;
+    default:
+      break;
+  }
+  console.log("SONUC:", sonuc);
+  return sonuc;
+};
+
+hesapla(3, 5, "+");
+console.log(hesapla(4, 3, "/"));
