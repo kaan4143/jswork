0379fa73369dc25179d77d2971fde1ea1b602a98
tuğrul kaan kılıç <tkaankilic@gmail.com>
2022-09-26 10:18:20 +0300

    APİ

--- api/async.js
+++ api/async.js
@@ -1,3 +1,25 @@
+//*=================================================
+//*               ASYNC-AWAIT
+//*=================================================
+//? Async-Await ECMAScript 2017 ile Javascript diline eklenmistir.
+//? Aslinda Promise yapisinin syntax olarak basitlestirilmis halidir.
+//? Bu baglamda sentetik seker benzetmesi yapilabilir.
+
+//* Bir fonskiyonu asenkron hale getirmek icin fonksiyon keyword'nun onune
+//* async keyword'u eklenir.
+
+//* Bir async fonksiyon icerisinde await keyword'u ile yapilan istegin cevabinin
+//* beklenmesi saglanir.
+
+//* Aslinda dizilis olarak senkron mantiga benzeyen kod yazarak Asenkron
+//* kod yazmayı mumkun kilar.
+
+//* Await, promise-temelli herhangi bir fonksiyonun onune getirilerek getirildigi
+//* satirdaki kodun durudurulmasini saglar. Yapilan istek yerine getirilip sonuc
+//* degerlerinin dondurulmesine ile kodun calismasi devam eder.
+
+let isError = false;
+
 const getNews = async function () {
   const API_KEY = "a8425c650c92438dbba8ecbd122e69d7";
 
@@ -18,7 +40,30 @@ const getNews = async function () {
   }
 };
 
+const renderNews = (news) => {
+  const newsList = document.getElementById("news-list");
+  if (isError) {
+    newsList.innerHTML += `
+      <h2>News Can not be fetched</h2>
+      <img src="./img/404.png" alt="" />
+    `;
+    return;
+  }
+  news.forEach((item) => {
+    const { title, description, urlToImage, url } = item; //! dest
+    newsList.innerHTML += `
+    <div class="col-md-6 col-lg-4 col-xl-3">
+      <div class="card">
+        <img src="${urlToImage}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text">${description}</p>
+          <a href="${url}" target="_blank" class="btn btn-danger">Details</a>
+        </div>
+      </div>
+    </div>
+    `;
+  });
+};
 
-
-
-getNews()
+window.addEventListener("load", getNews);
